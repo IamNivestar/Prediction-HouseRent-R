82e2f21209d8db58739b270e36a6ccdfb597a5cb
Amaury Ribeiro <amaurymariorneto@outlook.com>
2022-11-29 16:12:09 -0300

    resultados e primeiro modelo

--- house_rent_prediction.R
+++ house_rent_prediction.R
@@ -72,12 +72,20 @@ str(df)
 
 ## plotando gráficos e informações sobre os dados ##
 
+install.packages("gridExtra")
+library(gridExtra)
+
+
 #aluguel média por cidade
 Average_rent <- df %>%
   group_by(City) %>%
   summarise(mean(Rent))
 
 View(Average_rent)
+
+png("average_rent_city.png", height = 50*nrow(Average_rent), width = 200*ncol(Average_rent))
+grid.table(Average_rent)
+dev.off()
 
 summary (size)
 
@@ -87,23 +95,33 @@ library(ggplot2)
 count_floor <- table(df$Total_floor)
 View(count_floor)
 
-par(mar=c(3, 3, 3, 1))
+par(mar=c(3, 3, 3, 1)) #customizando barplot
 barplot(count_floor, ) #como esperado, imóveis com poucos andares são bem mais frequentes
 
 ggplot(as.data.frame(table(count_floor)), aes(x=count_floor, y= Freq))+
   geom_bar(stat = 'identity')
 
+quantile(df$Rent)
+?hist
+png(file="histogram_rent_90p.png", width=700, height=700, res=100)
+hist(df$Rent[ df$Rent < quantile(df$Rent, 0.90)], breaks = 5, labels = T, col='orange', main="Histogram of Rent", xlab =  "Rent")
+dev.off();
 
 ### Machine Learning ###
-# separação treinamento e teste #
+
 
 dim(df)
 
-set.seed(4)
-sample()
+#preprocessamento, mudando tipo das colunas
 
-samples_rows <- sample(1:length(df$BHK), length(df$BHK)*0.7)
-train = df[samples_rows,]  # pegando 70% para treino
+df$Floor <- as.factor(df$Floor)
+df$Area_locality <- as.factor((df$Area_locality))
+
+# separação treinamento e teste #
+
+set.seed(4) #definindo semente pois vou buscar valores aleatórios
+samples_rows <- sample(1:length(df$BHK), length(df$BHK)*0.7) # 70% para treino
+train = df[samples_rows,]  # dados de treino
 View(train)
 
 teste = df[-samples_rows,] # o restante é teste
@@ -129,4 +147,17 @@ modelo <- rpart( Rent ~ .,
                  data= train,
                  control = rpart.control(cp=0))
 
+
 ## Previsões e Resultados ##
+
+
+teste$predicted <- predict(modelo, teste)
+View(teste)
+
+# Avaliando resultados
+teste$porcent_error <- round(teste$predicted / teste$Rent, 2)
+teste$porcent_error <- abs(teste$porcent_error-1)
+
+Error_sum <- summary(teste$porcent_error)
+Error_sum
+
